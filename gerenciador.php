05b6728d33788f9e0fe19925db5981966d1b12a7
Adriana Diniz <dri.ragazoni@gmail.com>
2018-11-14 02:02:12 -0200

    Add files via upload

--- gerenciador.php
+++ gerenciador.php
@@ -7,28 +7,102 @@
 * Author URI: http://www.adrianaeanna.com.br
 * License: CC BY
 */
+//_FILE_ constante (arquivo)
 
- register_activation_hook(__FILE__, 
-                            'gerenciador_de_treinos');
+register_activation_hook( __FILE__ , 'criar_tabelas_gerenciador');
 
-function gerenciador_de_treinos(){
+function criar_tabelas_gerenciador(){
+	
 	global $wpdb;
-	global $table_prefix;
 
-	$wpdb -> query("CREATE TABLE {$table_prefix}gerenciador_de_treinos(id NULL AUTO_INCREMENT PRIMARY KEY,
-																		nome VARCHAR(30) NOT NULL,
-																		atividade VARCHAR(20) NOT NULL,
-																		data DATE NOT NULL,
-																		pace INT NOT NULL,
-																		distancia INT NULL,
-																		calorias INT NULL,
-																		carga_horaria INT");
+	$nome_tabela = $wpdb->prefix . 'gerenciador_treinos';
+
+	$sql = 'CREATE TABLE ' . $nome_tabela . '(
+		nome VARCHAR(255) NOT NULL,
+		atividade VARCHAR(255) NOT NULL,
+		data DATE,
+		pace INT NOT NULL,
+		distancia INT NOT NULL,
+		calorias INT NOT NULL,
+		carga_horaria INT NOT NULL);';
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
+
+		}
+
+//Tela para gravar diploma dentro do wp-admin
+add_action('admin_menu', 'tela_gravar_treino');
+function tela_gravar_treino(){ 
+
+	add_submenu_page( 	'options-general.php',
+						'Gerenciado de Treinos',
+						'Gravar Treino',
+						'administrator',
+						'validador-de-treino-config',
+						'pagina_para_gravar_treino');
+}
+
+function pagina_para_gravar_treino(){
+
+    global $wpdb;
+    global $table_prefix;
+
+    if(isset($_POST['nome_usuario']) && 
+		isset($_POST['data']) &&
+		isset($_POST['nome_atividade']) &&
+		isset($_POST['carga_hora']) && 
+		isset($_POST['calorias']) &&
+		isset($_POST['pace']) &&
+    	isset($_POST['km'])){
+        
+        if(!empty($_POST['nome_usuario']) && 
+			!empty($_POST['data']) &&
+			!empty($_POST['nome_atividade']) &&
+			!empty($_POST['carga_hora']) &&
+			!empty($_POST['calorias']) &&
+			!empty($_POST['pace']) &&
+			!empty($_POST['km'])){
+            
 
 
-	$page_title = 'Gerenciador de treinos';
+            $cod_autenticacao = md5(microtime());
 
-	$page = get_page_by_title($page_title);
+            if($wpdb->query("INSERT INTO 
+									{$table_prefix}validador_de_diploma
+									( nome,
+									  curso,
+									  data,
+									  cod_autenticacao)
+									VALUES
+									( 
 
-	//
+										'{$_POST['nome_do_aluno']}',
+										'{$_POST['nome_do_curso']}',
+										'{$_POST['emitido_em']}',
+										'$cod_autenticacao')")) {
+
+					$msg = "Código gerado: $cod_autenticacao";					
+				
+			}else{
+					$msg = 'Erro ao gravar diploma';
+			}
+
+
+        } else {
+            $msg = 'Os campos são obrigatórios';
+        }
+    }
+
+    $diplomas = $wpdb->get_results("SELECT
+    									nome,
+    									curso,
+    									data,
+    									cod_autenticacao
+    									FROM {$table_prefix}validador_de_diploma");
+    									
+    
+    include('template_config.php');
+
 }
 ?>
--- template_config.php
+++ template_config.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -16,25 +16,32 @@
 <form method="post">
 <div class="form-group">
   <label for="nome"></label>
-  <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
+  <input type="text" class="form-control" name="nome_usuario" id="nome" placeholder="Nome">
+  <br><br>
 
   <label for="data"></label>
   <input type="date" class="form-control" name="data" id="data" placeholder="Data">
+  <br><br>
 
   <label for="atividade"></label>
-  <input type="text" class="form-control" name="nome" id="atividade" placeholder="Atividade">
+  <input type="text" class="form-control" name="nome_atividade" id="atividade" placeholder="Nome Atividade">
+  <br><br>
 
   <label for="carga-hora"></label>
-  <input type="number" class="form-control" name="carga-hora" id="c-hora" placeholder="Carga Horaria">
+  <input type="number" class="form-control" name="carga_hora" id="c-hora" placeholder="Carga Horaria">
+  <br><br>
 
   <label for="calorias"></label>
   <input type="text" class="form-control" name="calorias" id="calorias" placeholder="Calorias">
+  <br><br>
 
   <label for="pace"></label>
   <input type="number" class="form-control" name="pace" id="pace" placeholder="Pace">
+  <br><br>
 
   <label for="km"></label>
-  <input type="text" class="form-control" name="km" id="km" placeholder="Kilometragem percorrida">
+  <input type="text" class="form-control" name="km" id="km" placeholder="Km">
+  <br><br>
 
   <?php submit_button();?>
   
